65eb5f74e9f481fffc9a49af19d4c32362e3b3af
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-08-19 23:26:43 +0700

    Information

---
+++ app/src/main/java/com/example/bersihnesia/activity/InformationActivity.java
@@ -0,0 +1,15 @@
+package com.example.bersihnesia.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.bersihnesia.R;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}

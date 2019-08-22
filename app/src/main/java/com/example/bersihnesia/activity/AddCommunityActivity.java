1e65fa717a32854215aa72ce013bf7cdc0ac3639
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-08-22 23:19:55 +0700

    fsdf

--- app/src/main/java/com/example/bersihnesia/activity/AddCommunityActivity.java
+++ app/src/main/java/com/example/bersihnesia/activity/AddCommunityActivity.java
@@ -3,6 +3,8 @@ package com.example.bersihnesia.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.bersihnesia.R;
+
 public class AddCommunityActivity extends AppCompatActivity {
 
     @Override

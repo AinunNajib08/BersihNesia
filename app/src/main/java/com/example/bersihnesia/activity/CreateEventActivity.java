faa737c8e2a8b21de16a54b7ddb3af7d880e1a0a
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-08-27 15:30:31 +0700

    kdkjkad

--- app/src/main/java/com/example/bersihnesia/activity/CreateEventActivity.java
+++ app/src/main/java/com/example/bersihnesia/activity/CreateEventActivity.java
@@ -1,15 +1,122 @@
 package com.example.bersihnesia.activity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
 
 import com.example.bersihnesia.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+
 public class CreateEventActivity extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private SimpleTimeZone simpleTimeZone;
+    ImageButton datePick;
+    EditText ettDate;
+    private TimePickerDialog timePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+
+        datePick = findViewById(R.id.btnDatePick);
+        ettDate = findViewById(R.id.ettDate);
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        datePick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimeDialog();
+            }
+        });
+
+
+
+    }
+
+    private void showTimeDialog() {
+
+        /**
+         * Calendar untuk mendapatkan waktu saat ini
+         */
+        Calendar calendar = Calendar.getInstance();
+
+        /**
+         * Initialize TimePicker Dialog
+         */
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                /**
+                 * Method ini dipanggil saat kita selesai memilih waktu di DatePicker
+                 */
+                ettDate.setText("Waktu dipilih = "+hourOfDay+":"+minute);
+            }
+        },
+                /**
+                 * Tampilkan jam saat ini ketika TimePicker pertama kali dibuka
+                 */
+                calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE),
+
+                /**
+                 * Cek apakah format waktu menggunakan 24-hour format
+                 */
+                DateFormat.is24HourFormat(this));
+
+        timePickerDialog.show();
+    }
+
+    private void showDateDialog(){
+
+        /**
+         * Calendar untuk mendapatkan tanggal sekarang
+         */
+        Calendar newCalendar = Calendar.getInstance();
+
+        /**
+         * Initiate DatePicker dialog
+         */
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                /**
+                 * Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
+                 */
+
+                /**
+                 * Set Calendar untuk menampung tanggal yang dipilih
+                 */
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                /**
+                 * Update TextView dengan tanggal yang kita pilih
+                 */
+                ettDate.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        /**
+         * Tampilkan DatePicker dialog
+         */
+        datePickerDialog.show();
     }
 }
